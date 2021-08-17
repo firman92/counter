1f2e7ff9853dd513a7b68c8bf6e3f35f5e9d1431
firman92 <firman92@live.fr>
2021-08-17 15:55:14 +0200

    finish exo 1 React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./compoments/Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Counter />
+        <h1>Counter</h1>
+        <h2>{this.state.count}</h2>
+        <button
+          className="plus"
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          +
+        </button>
+        <button
+          className="moins"
+          onClick={() => {
+            this.setState({ count: this.state.count - 1 });
+          }}
+        >
+          -
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
